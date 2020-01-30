6f0c3cbeee1cfcbe33d3cfa5a273f0b5b7c9609c
anmariya <anmariya1915@gmail.com>
2020-01-29 19:53:35 -0500

    [ANMARIYA]added codes in initial function

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -15,80 +15,77 @@ import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 public class LambtonStringTools 
 {
     //1 - REVERSE STRING
-    public static String reverse(String s)
-    {
-        String orginalString=s;
-        for (int i = 0; i < orginalString.length(); i++)
-        {
-            System.out.println(orginalString/*[i]*/);
+    public static String reverse(String s) {
+        String orginalString = s;
+        for (int i = 0; i < orginalString.length(); i++) {
+            System.out.println(orginalString);
         }
-        String reverseStrings[]=new String[orginalString.length()];
+        String reverseStrings[] = new String[orginalString.length()];
         String[] rstr;
-        rstr=new String[10];
-        for(int i=0;i<orginalString.length();i++)
-        {
-            String rev = reverseStrings(orginalString/*[i]*/);
-            reverseStrings[i]=rev;
-            rstr[i]=rev;
-            System.out.println(new String(orginalString/*[i]*/)+rev);
+        rstr = new String[10];
+        for (int i = 0; i < orginalString.length(); i++) {
+            String rev = reverseStrings(orginalString);
+            reverseStrings[i] = rev;
+            rstr[i] = rev;
+            System.out.println(new String(orginalString) + rev);
         }
+        return null;
     }
-    public static String reverseStrings(String s)
-    {
-        String temp=new String();
-        char names[]=s.toCharArray();
-        int len=names.length;
-        for(int i=0,j=len-1;i<len/2;i++,j--)
+
+        public static String reverseStrings (String s)
         {
-            char t=names[i];
-            names[i]=names[j];
-            names[j]=t;
+            String temp = new String();
+            char names[] = s.toCharArray();
+            int len = names.length;
+            for (int i = 0, j = len - 1; i < len / 2; i++, j--) {
+                char t = names[i];
+                names[i] = names[j];
+                names[j] = t;
+            }
+            temp = new String(names);
+            return temp;
         }
-        temp=new String(names);
-        return temp;
-    }
-}
-        //return null;
 
 
     //2 - FORMAT INITIALS OF STRING
-    public static String initials(String s) 
+   public static String initials(String s)
     {
         String fullName=s;
+        char names[] = fullName.toCharArray();
         int len=fullName.length();
+        for(int i=0;i<len;i++){
+            if(names[i]!='\0'){
+                char initial =names[i];
+            }
+        }
 
         return null;
-    }
+    }*/
         
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
-    public static String mostFrequent(String s) 
+    /*public static String mostFrequent(String s)
     {
         return null;
-    }
+    }*/
     
     //4 - CONVERT BINARY NUMBER TO DECIMALS
-    public static int binaryToDecimal(String s) 
+    public static int binaryToDecimal(int s)
     {
-        String binaryNumber=s;
+        int binaryNumber=s;
         int decimal=0;
-        int len=binaryNumber.length();
-        for(int i=0,;i<len/2;i++,i++)
+        for(int i=0;i++)
         {
             int temp = binaryNumber%10;
             decimal += temp*2;
             binaryNumber = binaryNumber/10;
         }
-        }
 
-        /*for(int i=0;i<binaryNumber.length();i++){
-
-        }*/
-      // return 0;
     }
-     
+
     //5 - REPLACING SUBSTRING WITH NEW STRING IN EXISTING STRING
-    public static String replaceSubString(String originalString, String findString, String newString) 
+    /*public static String replaceSubString(String originalString, String findString, String newString)
     {
         return null;
-    }
+    }*/
 }
+
